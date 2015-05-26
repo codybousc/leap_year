56164b79dc6999e5f1fceacdd9c42790a2f5aa60
cody bouscaren & franz knupfer <student@epicodus.com>
2015-05-26 10:43:23 -0700

    completes leap year function

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,33 @@
 var leapYear = function(year) {
-  return false;
+  if (year % 400 === 0) {
+    return true;
+  }
+
+  else if ((year % 100 !== 0) && (year % 4 ===0)) {
+    return true;
+  }
+
+  else {
+    return false;
+  }
 };
+
+$(document).ready(function() {
+  $("form#leap-year").submit(function(event) {
+    var year = parseInt($("input#year").val());
+    var result = leapYear(year);
+
+    $(".year").text(year);
+    if (result) {
+      $(".not").text("")
+    }
+
+    else if (!result) {
+      $(".not").text("not");
+    }
+
+    $("#result").show();
+
+    event.preventDefault();
+  });
+});
